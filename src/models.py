9f21512fb9ebc9f1c15e1b7f3ebae93c7392d116
Yasas <wayasas@gmail.com>
2017-03-16 21:40:00 +0530

    Finished the end to end predicting process. Need a clear code with isolation of code segments by function.

---
+++ src/models.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 16 15:47:15 2017
+
+@author: Yasas
+"""
+#
+# Load libraries
+# import pandas
+# from pandas.tools.plotting import scatter_matrix
+# import matplotlib.pyplot as plt
+# from sklearn.metrics import classification_report
+# from sklearn.metrics import confusion_matrix
+# from sklearn.metrics import accuracy_score
+from sklearn import model_selection
+# from sklearn.linear_model import LogisticRegression
+# from sklearn.tree import DecisionTreeClassifier
+# from sklearn.neighbors import KNeighborsClassifier
+# from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+# from sklearn.naive_bayes import GaussianNB
+# from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+#
+# Input transforms
+#
+# Spot Check Algorithms
+models = []
+# models.append(('LR', LogisticRegression()))
+# models.append(('LDA', LinearDiscriminantAnalysis()))
+# models.append(('KNN', KNeighborsClassifier()))
+# models.append(('CART', DecisionTreeClassifier()))
+# models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC()))
+#
+# evaluate each model in turn
+def cross_val(features, labels, seed = 7, scoring = 'f1'):
+    results = []
+    names = []
+    for name, model in models:
+        kfold = model_selection.KFold(n_splits=20, random_state=seed)
+        cv_results = model_selection.cross_val_score(model, features, labels, cv=kfold, scoring=scoring)
+        results.append(cv_results)
+        names.append(name)
+        msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+        print(msg)
--- src/read_data.py
+++ src/read_data.py
@@ -1,22 +1,100 @@
 # Load libraries
 import re
 import pandas as pd
-
+import numpy as np
+from bs4 import BeautifulSoup
+from nltk.corpus import stopwords # Import the stop word
+from sklearn.feature_extraction.text import CountVectorizer
 # Custom imports
+import models
+# Transforms review text to words
+def review_to_words( text ):
+    # Function to convert a raw review to a string of words
+    # The input is a single string (a raw movie review), and 
+    # the output is a single string (a preprocessed movie review)
+    #
+    # 1. Remove HTML
+    review_text = BeautifulSoup(text, "lxml").get_text() 
+    #
+    # 2. Remove non-letters        
+    letters_only = re.sub("[^a-zA-Z]", " ", review_text) 
+    #
+    # 3. Convert to lower case, split into individual words
+    words = letters_only.lower().split()                             
+    #
+    # 4. In Python, searching a set is much faster than searching
+    #   a list, so convert the stop words to a set
+    stops = set(stopwords.words("english"))                  
+    # 
+    # 5. Remove stop words
+    meaningful_words = [w for w in words if not w in stops]
+    #
+    # 6. Join the words back into one string separated by space, 
+    # and return the result.
+    return( " ".join( meaningful_words ))
 
-# Load dataset
-url = "../data/csv/ABSA16_Restaurants_Train_SB1_v2.csv" # relative dataset URL
-dataset = pd.read_csv(url, encoding = 'latin1') # reads dataset with headers
-
-train = dataset
-        .groupby('text', as_index=False)['category']
-        .agg({'categories':(lambda x: list(x))})
-
-# Clean the dataset
-def clean(text):
-    return re.sub("[^a-zA-Z]",      # The pattern to search for
-                  " ",              # The pattern to replace it with
-                  train['text'].get_text())    # The text to search
-
-
-# 
+if __name__ == "__main__":
+    # Load dataset
+    url = "../data/csv/ABSA16_Restaurants_Train_SB1_v2.csv" # relative dataset URL
+    dataset = pd.read_csv(url, encoding = 'latin1') # reads dataset with headers
+    #
+    train = dataset.groupby('text', as_index=False)['category'].agg({'categories': (lambda x: list(x))})
+    #
+    # Get the number of reviews based on the dataframe column size
+    num_reviews = train["text"].size
+    #
+    # Initialize an empty list to hold the clean reviews
+    clean_train_reviews = []
+    #
+    # Loop over each review; create an index i that goes from 0 to the length
+    # of the movie review list 
+    print("Cleaning and parsing the training set review sentences...")
+    for i in range( 0, num_reviews ):
+        if( (i + 1) % 250 == 0 ):
+            print("\tReview sentence %d of %d" % ( i + 1, num_reviews ))
+        # Call our function for each one, and add the result to the list of 
+        # clean reviews
+        clean_train_reviews.append( review_to_words( train["text"][i] ) )
+    #
+    print("\nCreating the bag of words...")
+    #
+    # Initialize the "CountVectorizer"  
+    vectorizer = CountVectorizer(analyzer = "word",
+                                 tokenizer = None,
+                                 preprocessor = None,
+                                 stop_words = None,
+                                 max_features = 5000)
+    # Scikit-learn's bag of words tool
+    train_data_features = vectorizer.fit_transform(clean_train_reviews)
+    # Numpy arrays are easy to work with, so convert the result to an array
+    train_data_features = train_data_features.toarray()
+    print(train_data_features.shape)
+    vocab = vectorizer.get_feature_names()
+    #
+    # Sum up the counts of each vocabulary word
+    dist = np.sum(train_data_features, axis=0)
+    # Define Categories
+    categories = []
+    categories.append(('resturentGeneral', 'RESTAURANT#GENERAL'))
+    categories.append(('resturentPrice', 'RESTAURANT#PRICES'))
+    categories.append(('resturentMiscellaneous', 'RESTAURANT#MISCELLANEOUS'))
+    for name, category in categories:
+        #
+        # For each, print the vocabulary word and the number of times it 
+        # appears in the training set
+        # for tag, count in zip(vocab, dist):
+        #   print(count, tag)
+        #
+        train[name] = train['categories'].apply(lambda x: category in x)
+        #
+        print("Training the SVM...")
+        # Initialize a Random Forest classifier with 100 trees
+        # forest = RandomForestClassifier(n_estimators = 100)
+        # Fit the forest to the training set, using the bag of words as 
+        # features and the sentiment labels as the response variable
+        #
+        # This may take a few minutes to run
+        # forest = forest.fit( train_data_features, train["resturentGeneral"] )
+        features = train_data_features
+        labels = train[name]
+        models.cross_val(features, labels);
